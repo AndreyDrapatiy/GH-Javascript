3320fbb91ae43c813cea10ea93425cc550c8c770
unknown <drapatiy92@gmail.com>
2017-10-09 16:53:00 +0300

    Homework 1 add

---
+++ lsn_1/script.js
@@ -0,0 +1,38 @@
+var arr0 = [3,0,-5,1,44,-12,3,0,0,1,2,-3,-3,2,1,4,-2-3-1];
+var arr1 = [-1,-12,-2];
+var arr2 = [1,7,3];
+var arr3 = [1,undefined,3,5,-3];
+var arr4 = [1,NaN,3,5,-3];
+
+function getMinMaxSum (argarr) {
+    var sum = null;
+    var min = argarr[0];
+    var max = argarr[0];
+
+    for (i = 0; i < argarr.length; i++){
+
+        if(!isNaN(argarr[i])){
+            sum = sum += argarr[i];
+        }
+        if (argarr[i] < min){
+            min = argarr[i];
+        }
+        if (argarr[i] > max ){
+            max = argarr[i];
+        }
+    }
+    return{
+        "Sum =" : sum,
+        "Min =" : min,
+        "Max =" : max
+    }
+}
+
+console.log(getMinMaxSum(arr0));
+console.log(getMinMaxSum(arr1));
+console.log(getMinMaxSum(arr2));
+console.log(getMinMaxSum(arr3));
+console.log(getMinMaxSum(arr4));
+
+
+
