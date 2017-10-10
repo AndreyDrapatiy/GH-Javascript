478830ea5f0300756d2170d6f8f7615591e9f1a8
unknown <drapatiy92@gmail.com>
2017-10-10 22:16:09 +0300

    some fixes && add new array for test

--- lsn_1/script.js
+++ lsn_1/script.js
@@ -2,7 +2,8 @@ var arr0 = [3,0,-5,1,44,-12,3,0,0,1,2,-3,-3,2,1,4,-2-3-1];
 var arr1 = [-1,-12,-2];
 var arr2 = [1,7,3];
 var arr3 = [1,undefined,3,5,-3];
-var arr4 = [1,NaN,3,5,-3];
+var arr4 = [1,NaN,3,5,-3, 'aaa'];
+var arr5 = [1, -4, NaN, 43, undefined, '2', 'djfl'];
 
 function getMinMaxSum (argarr) {
     var sum = null;
@@ -11,7 +12,7 @@ function getMinMaxSum (argarr) {
 
     for (i = 0; i < argarr.length; i++){
 
-        if(!isNaN(argarr[i])){
+        if(!isNaN(argarr[i]) && typeof(argarr[i]) === 'number'){
             sum = sum += argarr[i];
         }
         if (argarr[i] < min){
@@ -33,6 +34,7 @@ console.log(getMinMaxSum(arr1));
 console.log(getMinMaxSum(arr2));
 console.log(getMinMaxSum(arr3));
 console.log(getMinMaxSum(arr4));
+console.log(getMinMaxSum(arr5));
 
 
 
